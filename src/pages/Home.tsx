bf2a0a9a21351a2030fb8c472ca0f0df458b1088
SziNo <szino1848@gmail.com>
2022-10-11 11:00:46 +0200

    adding post and like

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { getDocs, collection } from 'firebase/firestore'
+import { db } from '../config/firebase'
+import Post from '../components/Post'
+
+export interface Post {
+  id: string
+  userId: string
+  title: string
+  username: string
+  description: string
+}
 
 const Home = () => {
-  return <div>Home</div>
+  const [postsList, setPostsList] = useState<Post[] | null>(null)
+
+  const postsRef = collection(db, 'posts')
+
+  const getPosts = async () => {
+    const data = await getDocs(postsRef)
+    setPostsList(
+      data.docs.map((doc) => ({ ...doc.data(), id: doc.id })) as Post[]
+    )
+  }
+
+  useEffect(() => {
+    getPosts()
+  }, [])
+  return (
+    <main>
+      {postsList?.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </main>
+  )
 }
 
 export default Home
